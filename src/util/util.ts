e16532ed7590ee3ac49e7133533b8f8b0f0f7d87
tanjataylor1 <78738562+tanjataylor1@users.noreply.github.com>
2021-03-02 20:16:14 -0500

    added screenshots

--- src/util/util.ts
+++ src/util/util.ts
@@ -8,6 +8,7 @@ import Jimp = require('jimp');
 //    inputURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
+
 export async function filterImageFromURL(inputURL: string): Promise<string>{
     return new Promise( async resolve => {
         const photo = await Jimp.read(inputURL);
